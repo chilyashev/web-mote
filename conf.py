3e3c89e63c574981d9c480a0d2f6c399767bc719
Mihail Chilyashev <m@chilyashev.com>
2015-05-20 20:45:46 +0300

    added the external disk

--- conf.py
+++ conf.py
@@ -1,5 +1,5 @@
 ## list of directories which contain things we might want to play
 root = ["/media/win",
-        "/home/inaimathi/music"]
+        "/media/hdd"]
 ## what port do you want the HTTP server to listen on
 httpport = 8080
